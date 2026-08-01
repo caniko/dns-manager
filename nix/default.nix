14e973a22d3da0112be53247f0ea2e3ea994ac72
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-02 00:03:40 +0200

    ci: publish flake inputs to Attic

--- nix/default.nix
+++ nix/default.nix
@@ -74,9 +74,22 @@ nix-manager-core.lib.mkManagerOutputs {
       '';
     });
 
-    apps = forAllSystems (system: {
+    apps = forAllSystems (system: let
+      pkgs = pkgsFor system;
+      atticAdapter = rs-harbor.lib.mkAdapter {
+        attic = {
+          endpoint = "https://attic.candee.baby";
+          cache = "canix";
+        };
+      };
+    in {
       deploy-pages = plinth.lib.${system}.mkDeployPagesApp {
         domain = "dns-manager.tartanoglu.com";
+      };
+      push-flake-inputs = rs-harbor.lib.mkAtticPush {
+        inherit pkgs;
+        adapter = atticAdapter;
+        flake = ".";
       };
     });
 
